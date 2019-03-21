bd0be7b03fd8bec9202a82acdf0394953d7dd400
BrodyJorgensen <jorgo9110@gmail.com>
2019-03-21 14:26:03 +1000

    refactoring the password task

--- password_checker.py
+++ password_checker.py
@@ -1,15 +1,18 @@
+
+
 def main():
-    # statements...
-    do_stuff()
 
-
-def do_stuff():
     min_password_length = 3
-    password = input("Password: ")
+    password = get_password()
     while min_password_length >= 0:
         if len(password) >= min_password_length:
             print(len(password) * '*')
-        password = input("Password: ")
+        password = get_password()
+
+
+def get_password():
+    password = input("Password: ")
+    return password
 
 
 # statements...
