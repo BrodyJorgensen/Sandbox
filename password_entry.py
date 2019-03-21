86a25bd690a25b6e56dc666b034c0c5e2836904f
BrodyJorgensen <jorgo9110@gmail.com>
2019-03-21 13:31:50 +1000

    finnished code

--- password_entry.py
+++ password_entry.py
@@ -1,1 +1,7 @@
 """Brody Jorgesen"""
+min_password_length = 3
+password = input("Password: ")
+while min_password_length >= 0:
+    if len(password) >= min_password_length:
+        print(len(password) * '*')
+    password = input("Password: ")
